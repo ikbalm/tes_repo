5a95b754c166ba6e0f682ef324923b1f6be876d5
ikbalm <iqb4l.mun4di1@gmail.com>
2016-05-03 11:22:20 +0700

    Added files via upload

---
+++ Simpan_Nilai.php
@@ -0,0 +1,28 @@
+<?
+include "Koneksi.PHP";
+
+		
+$hasil = mysql_query("insert into nilai (npm, kode, nilai, tanggal, keterangan) 
+				VALUES( 	'" . $_REQUEST[T_NPM] . "',
+ 							'" . $_REQUEST[T_NAMA] . "',
+							'" . $_REQUEST[T_NILAI] . "',
+							'" . format_tgl($_REQUEST["T_Tanggal"]) . "', 
+							'" . $_REQUEST[T_Keterangan] . "')");
+
+if ($hasil)
+{
+	header('location:View_Nilai_Beda_Warna.PHP');
+}
+else
+{
+	echo "Proses Simpan Gagal";
+}	
+
+
+function format_tgl($input)
+	{
+		$part = split("/", $input);
+		return $part[2] . "-" . $part[1] . "-" . $part[0];
+	}
+
+?>
